9da69e54c233c4c6be7d377dd6565716713205cb
kellysaraf <kellysaraf@me.com>
2026-03-01 10:13:09 -0300

    Create etl.py

---
+++ src/etl.py
@@ -0,0 +1,13 @@
+def extract():
+    print("Extraindo dados...")
+
+def transform():
+    print("Transformando dados...")
+
+def load():
+    print("Carregando dados tratados...")
+
+if __name__ == "__main__":
+    extract()
+    transform()
+    load()
