519d9a3cb0c06685888840cf9d85f95efd5254c3
kellysaraf <kellysaraf@me.com>
2026-03-22 21:42:43 -0300

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -1,13 +1,20 @@
+import pandas as pd
+
 def extract():
     print("Extraindo dados...")
+    df = pd.read_csv("data/raw/olist_orders_dataset.csv")
+    return df
 
-def transform():
+def transform(df):
     print("Transformando dados...")
+    df = df.dropna()
+    return df
 
-def load():
-    print("Carregando dados tratados...")
+def load(df):
+    print("Salvando dados tratados...")
+    df.to_csv("data/processed/orders_clean.csv", index=False)
 
 if __name__ == "__main__":
-    extract()
-    transform()
-    load()
+    df = extract()
+    df = transform(df)
+    load(df)
