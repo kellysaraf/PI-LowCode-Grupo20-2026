93c24bc1b529929f4a670344eb1210efd5ca5386
matheussilvalucas91 <matheus.silva.lucas91@gmail.com>
2026-05-05 18:08:24 -0300

    1 - Inclusão da dataset base e tratada

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,1 +1,15 @@
-//dashboard....
+import streamlit as st
+import pandas as pd
+
+st.title("Dashboard - E-commerce Olist")
+
+try:
+    df = pd.read_csv("data/processed/orders_clean.csv")
+
+    st.metric("Total de pedidos", len(df))
+
+    st.write("Dados:")
+    st.dataframe(df.head())
+
+except:
+    st.warning("Execute o ETL antes para gerar os dados.")
